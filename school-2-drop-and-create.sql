107a265b4f569838457986d43192aec8960c9353
Ilesh Jain <ilesh.jain@kellogg.northwestern.edu>
2024-04-12 18:57:53 +0000

    class 3

--- school-2-drop-and-create.sql
+++ school-2-drop-and-create.sql
@@ -1,3 +1,16 @@
 -- Drop the students table
 
+DROP TABLE students 
+;
+
 -- Recreate it
+
+CREATE TABLE students (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  first_name TEXT,
+  last_name TEXT,
+  email TEXT
+);
+
+
+--------issue with this: we deleted the data just to add a column
---
+++ school-6-update.sql
@@ -0,0 +1,3 @@
+UPDATE students 
+SET email = "janedoe@gmail.com"
+WHERE first_name = "Jane" and last_name = "Doe"
